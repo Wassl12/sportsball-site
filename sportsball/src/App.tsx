a30d528c8ccdb3ca70fff1fefcd99864c766e465
wassinkl <lukewassink88@gmail.com>
2023-06-12 20:13:45 -0500

    Add navbar

--- sportsball/src/App.tsx
+++ sportsball/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/kevins-corner">eSports</a>
+          <a className="navbar-brand" href="/baseball">Baseball </a>
+          <a className="navbar-brand" href="/tennis">Tennis </a>
+          <a className="navbar-brand" href="/about">About us</a>
+        </nav>
+        {/* Rest of your app */}
+      </div>
+    );
+  }
+  
+  export default App;
